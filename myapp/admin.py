e1858d1bb8e6995f1ee557e04276587fc5e161f0
Dvtung1305 <tung.210039@tbd.edu.vn>
2024-06-30 09:53:56 +0700

    login vs register

--- myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from myapp.models import Student
-
-# Register your models here.
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age', 'email')
-
-admin.site.register(Student, StudentAdmin)
